2f90089d7d86a587531f87c794c21a082ef20158
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-04-09 12:01:06 +0800

    completed adding todo functionality, building out toggle mode

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,48 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import { todoReducer, initialTodoState } from "./reducers/todoReducer";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todoState, dispatch] = useReducer(todoReducer, initialTodoState);
-  // console.log(todoState);
+  const [newTodoText, setNewTodoText] = useState("");
+
+  const onInputChange = e => {
+    setNewTodoText(e.target.value);
+  };
+
+  const onToggleCompleted = id => {
+    dispatch({
+      type: "TOGGLE_TODO",
+      // i need to send id data thru payload to tell reducer which todo to toggle!
+      payload: id
+    });
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      payload: { item: newTodoText, completed: false, id: uuidv4() }
+    });
+    setNewTodoText("");
+  };
 
   return (
     <div className="ui container">
-      <div className="ui checkbox">
-        <input type="checkbox" name="example" />
-        <label>{todoState.item}</label>
-      </div>
+      <h1>My Todo App</h1>
+      {todoState.map(todo => {
+        return (
+          <div key={todo.id}>
+            <p>{todo.item}</p>
+            <button onClick={onToggleCompleted}>Completed</button>
+          </div>
+        );
+      })}
+
+      <form onSubmit={onFormSubmit}>
+        <input type="text" value={newTodoText} onChange={onInputChange} />
+        <button>Add Todo</button>
+      </form>
     </div>
   );
 }
--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,15 +1,19 @@
-export const initialTodoState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 3892987589
-};
+import { v4 as uuidv4 } from "uuid";
+
+export const initialTodoState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: uuidv4()
+  }
+];
 
 export const todoReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return { ...state, item: action.payload };
+      return [...state, action.payload];
     case "TOGGLE_TODO":
-      return { ...state, completed: !state.completed };
+      return [...state, { item: action.payload }];
     case "CLEAR_COMPLETED":
       return; // use array method to check condition of completed, then deleting those
     default:
